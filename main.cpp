f316ece088d2cabf51bb9e86c6ae2f007e9e2e52
DylShero <165214379+DylShero@users.noreply.github.com>
2025-11-28 20:56:35 +0000

    Added some additional comments to code

--- main.cpp
+++ main.cpp
@@ -9,21 +9,26 @@ int main(int argc, char** argv) {
     std::cout << "Running main\n";
     Mat image = imread("C:/Users/dsdyl/Pictures/jmonkg.png",IMREAD_COLOR);
 
-    uint8_t* pixelPtr = (uint8_t*)image.data;
-    int pixelCount = image.rows * image.cols;
-
-    for(int i = 0; i < pixelCount; i++) {
-    pixelPtr[i] = pixelPtr[i] + 100;
-    }
-
+    //Image error checking.
     if(image.empty()){
         std::cout << "Image not found\n";
         std::cin.get();
         return(-1);
     }
 
+    //Define pointers to access individual pixels
+    uint8_t* pixelPtr = (uint8_t*)image.data;
+
+    int pixelCount = image.rows * image.cols;
+
+    //Iterate accross each pixel.
+    for(int i = 0; i < pixelCount; i++) {
+    pixelPtr[i] = pixelPtr[i] + 100; //Modifies look of each pixel
+    }
+
+    //Displays image 
     imshow("First image", image);
-    waitKey(0);
+    waitKey(0); //Waits with image open
     std::cout << "Hello World!\n";
     return 0;
 }
